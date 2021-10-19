680db17ca757064b0c9e26279249720f3c3a9263
IIDEAT <79359382+IIDEAT@users.noreply.github.com>
2021-10-19 17:47:19 +0800

    Add files via upload

---
+++ js/read_score.js
@@ -0,0 +1,41 @@
+AV.init({
+  appId: "1wFqGRggvkflvsHK71yBeoLp-gzGzoHsz",
+  appKey: "wHzWdO649dBr7q09CuunEv5i",
+  serverURL: "https://1wfqgrgg.lc-cn-n1-shared.com"
+});
+
+var query = new AV.Query('zhuoban');
+  query.find().then( (boards)=> {
+      let array = boards.map((xxx)=>xxx.attributes)
+      
+      array.forEach((yyy =>{
+        console.log(yyy.name)
+          // let li = document.createElement('li')
+          // li.innerText = yyy.name
+          // let boardlist = document.querySelector('#messageList')
+          // boardlist.appendChild(li)
+      }))
+    },function (error) {
+     alert('提交失败，改天再来')
+  });
+
+// let myForm = document.querySelector('#postMessage')
+// myForm.addEventListener('submit',(e)=>{
+//     e.preventDefault()
+//     let name = myForm.querySelector('input[name=name]').value;
+//     let content = myForm.querySelector('input[name=content]').value
+//     var Message = AV.Object.extend('Message');
+//     var message = new Message();
+//     message.save({
+//          'name':name,
+//          'content': content
+//       }).then((object)=>{
+//           let li = document.createElement('li')
+//           li.innerText = `${object.attributes.name} : ${object.attributes.content}`
+//           let messageList = document.querySelector('#messageList')
+//           messageList.appendChild(li)
+//           myForm.querySelector('input[name=name]').value = ''
+//           myForm.querySelector('input[name=content]').value = ''
+
+//       })
+// })
