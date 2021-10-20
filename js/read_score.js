f78f200ab9ca39938cd4b2931c0ec8fb7fa3c31f
IIDEAT <79359382+IIDEAT@users.noreply.github.com>
2021-10-20 16:12:26 +0800

    Update read_score.js

--- js/read_score.js
+++ js/read_score.js
@@ -1,39 +1,48 @@
 AV.init({
-  appId: "1wFqGRggvkflvsHK71yBeoLp-gzGzoHsz",
-  appKey: "wHzWdO649dBr7q09CuunEv5i",
-  serverURL: "https://1wfqgrgg.lc-cn-n1-shared.com"
+    appId: "1wFqGRggvkflvsHK71yBeoLp-gzGzoHsz",
+    appKey: "wHzWdO649dBr7q09CuunEv5i",
+    serverURL: "https://1wfqgrgg.lc-cn-n1-shared.com"
 });
 
+function display(name, name_cn){
+    let div = document.createElement('div')
+    div.style.background = "#F8F8F5";
+    div.style.borderStyle = "solid";
+    div.style.borderColor = "#D4D4D4";
+    div.style.borderWidth = "1px";
+    div.style.height = "80%";
+    div.style.width = "20%";
+}
 var query = new AV.Query('zhuoban');
-  query.find().then( (boards)=> {
-      let array = boards.map((xxx)=>xxx.attributes)
-      
-      array.forEach((yyy =>{
-          let a = document.createElement('a')
-          let ul = document.createElement('ul')
-          let li_name = document.createElement('li')
-          li_name.className = 'ProTi';
-          li_name.innerText = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + yyy.name_cn;
-          ul.appendChild(li_name);
-          let li = document.createElement('li')
-          li.innerText = "作\xa0\xa0\xa0\xa0\xa0\xa0者：" + yyy.author;
-          ul.appendChild(li);
-          let li2 = document.createElement('li')
-          var score = ((yyy.nandu + yyy.suiji + yyy.yule + yyy.celue + yyy.dairu + yyy.hudong + yyy.meishu) / 7 ).toFixed(2);
-          console.log(score);
-          li2.innerText = "桌瓣评分：" + score.toString();
-          ul.appendChild(li2);
-          a.rel = 'src/pic/' + yyy.name + '.png'
-          console.log(a.rel)
-          a.href = 'http://sc.chinaz.com/'
-          a.appendChild(ul);
-          document.getElementById('ddd').appendChild(a);
-          // let li = document.createElement('li')
-          // li.innerText = yyy.name
-          // let boardlist = document.querySelector('#messageList')
-          // boardlist.appendChild(li)
-      }))
-    });
+query.find().then( (boards)=> {
+    let array = boards.map((xxx)=>xxx.attributes)
+
+    array.forEach((yyy =>{
+        let a = document.createElement('a')
+        let ul = document.createElement('ul')
+        let li_name = document.createElement('li')
+        li_name.className = 'ProTi';
+        li_name.innerText = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + yyy.name_cn;
+        ul.appendChild(li_name);
+        let li = document.createElement('li')
+        li.innerText = "作\xa0\xa0\xa0\xa0\xa0\xa0者：" + yyy.author;
+        ul.appendChild(li);
+        let li2 = document.createElement('li')
+        var score = ((yyy.nandu + yyy.suiji + yyy.yule + yyy.celue + yyy.dairu + yyy.hudong + yyy.meishu) / 7 ).toFixed(2);
+        console.log(score);
+        li2.innerText = "桌瓣评分：" + score.toString() + " (" + yyy.num.toString() + "人评分)";
+        ul.appendChild(li2);
+        a.rel = 'src/pic/' + yyy.name + '.png'
+        a.onclick = Function(display(yyy.name, yyy.name_cn))
+        a.href = 'http://sc.chinaz.com/'
+        a.appendChild(ul);
+        document.getElementById('ddd').appendChild(a);
+        // let li = document.createElement('li')
+        // li.innerText = yyy.name
+        // let boardlist = document.querySelector('#messageList')
+        // boardlist.appendChild(li)
+    }))
+});
 
 // let myForm = document.querySelector('#postMessage')
 // myForm.addEventListener('submit',(e)=>{
